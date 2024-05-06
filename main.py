3dbe5214ac7e77daee32eb09123fa9e6ed62dc43
masygreen <masygreen@gmail.com>
2024-05-06 17:54:10 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+
+
+def get_mov_timestamps(filename):
+    ''' Get the creation and modification date-time from .mov metadata.
+
+        Returns None if a value is not available.
+    '''
+    from datetime import datetime as DateTime
+    import struct
+
+    ATOM_HEADER_SIZE = 8
+    # difference between Unix epoch and QuickTime epoch, in seconds
+    EPOCH_ADJUSTER = 2082844800
+
+    creation_time = modification_time = None
+
+    # search for moov item
+    with open(filename, "rb") as f:
+        while True:
+            atom_header = f.read(ATOM_HEADER_SIZE)
+            #~ print('atom header:', atom_header)  # debug purposes
+            if atom_header[4:8] == b'moov':
+                break  # found
+            else:
+                atom_size = struct.unpack('>I', atom_header[0:4])[0]
+                f.seek(atom_size - 8, 1)
+
+        # found 'moov', look for 'mvhd' and timestamps
+        atom_header = f.read(ATOM_HEADER_SIZE)
+        if atom_header[4:8] == b'cmov':
+            raise RuntimeError('moov atom is compressed')
+        elif atom_header[4:8] != b'mvhd':
+            raise RuntimeError('expected to find "mvhd" header.')
+        else:
+            f.seek(4, 1)
+            creation_time = struct.unpack('>I', f.read(4))[0] - EPOCH_ADJUSTER
+            creation_time = DateTime.fromtimestamp(creation_time)
+            if creation_time.year < 1990:  # invalid or censored data
+                creation_time = None
+
+            modification_time = struct.unpack('>I', f.read(4))[0] - EPOCH_ADJUSTER
+            modification_time = DateTime.fromtimestamp(modification_time)
+            if modification_time.year < 1990:  # invalid or censored data
+                modification_time = None
+
+    return creation_time, modification_time
+
+
+def main():
+    for root, dirs, files in os.walk(move_folder):
+        for file in files:
+            if file.lower().endswith('.mov'):
+                v1, v2 = get_mov_timestamps(os.path.join(root, file))
+                old_file_name = os.path.join(root, file) # old file name
+                new_file_name = os.path.join(root, f"{v1.strftime('%Y%m%d')} {file.strip()}") # new file name
+                print(f"{old_file_name} => {new_file_name}")
+                os.rename(old_file_name, new_file_name)
+
+if __name__ == '__main__':
+    move_folder = os.getcwd()
+    main()
